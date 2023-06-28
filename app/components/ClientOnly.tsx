12d50d34eaf704259db4cab754b4c7512aec853f
eren <erenbezirganci99@gmail.com>
2023-06-29 01:23:14 +0300

    hydration error fix

---
+++ app/components/ClientOnly.tsx
@@ -0,0 +1,14 @@
+"use client"
+import {use, useEffect, useState} from "react";
+interface ClientOnlyProps {
+    children: React.ReactNode;
+}
+const ClientOnly: React.FC<ClientOnlyProps> = ({children}) => {
+    const [isMounted, setIsMounted] = useState(false);
+    useEffect(() => {setIsMounted(true)}, []);
+    if(!isMounted) return null;
+    
+    return ( <>{children}</>  );
+}
+ 
+export default ClientOnly;
